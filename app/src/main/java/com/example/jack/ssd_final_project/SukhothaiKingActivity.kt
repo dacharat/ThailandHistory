49225415267ab4a397fdca0a149436036a1faa7e
dacharat <dacharat.p@ku.th>
2018-05-19 18:40:13 +0700

    :iphone: Add json file

---
+++ app/src/main/java/com/example/jack/ssd_final_project/SukhothaiKingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jack.ssd_final_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SukhothaiKingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sukhothai_king)
+    }
+}
